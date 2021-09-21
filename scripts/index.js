52fc9065a629fff27b3b16007eddb79b700572ff
Grave <kyuvcpavm@mail.ru>
2021-09-21 12:42:34 +0300

    first fixes

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,12 @@
 const popup = document.querySelector('.popup');
 const popupOpenBtn = document.querySelector('.profile__btn-edit');
 const popupCloseBtn = popup.querySelector('.popup__close');
-const like = document.querySelectorAll('.element__btn-like');
 let form = popup.querySelector('.popup__container');
 let nameInput = popup.querySelector('.popup__name');
 let jobInput = popup.querySelector('.popup__job');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__description');
 
-for(let i = 0; i<like.length; i++) {
-  like[i].addEventListener('click', function(){
-    like[i].classList.toggle('element__btn-like_active')
-  }
-  )
-}
 function popupToggle() {
   popup.classList.toggle('popup__opened')
   nameInput.value = profileName.textContent;

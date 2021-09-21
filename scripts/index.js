7e003bb9d45617fdb86d9839794aacc7e4731e0a
Grave <kyuvcpavm@mail.ru>
2021-09-21 17:38:56 +0300

    new

--- scripts/index.js
+++ scripts/index.js
@@ -2,33 +2,33 @@ const popup = document.querySelector('.popup');
 const popupOpenBtn = document.querySelector('.profile__btn-edit');
 const popupCloseBtn = popup.querySelector('.popup__close');
 let form = popup.querySelector('.popup__container');
-let nameInput = popup.querySelector('.popup__name');
-let jobInput = popup.querySelector('.popup__job');
+let nameInput = popup.querySelector('.popup__info_type_name');
+let jobInput = popup.querySelector('.popup__info_type_job');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__description');
 
-function popupOpen() {
-  popup.classList.add('popup__opened')
+function openPopup() {
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
+  popup.classList.add('popup__opened');
 };
-function popupClose() {
-  popup.classList.remove('popup__opened')
+function closePopup() {
+  popup.classList.remove('popup__opened');
 };
 
 function clickOverlay(event){
   if (event.target === event.currentTarget) {
-    popupClose()
+    closePopup();
   }
 }
 function formSubmitHandler (evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
-  popupClose()
+  closePopup();
 }
 
 form.addEventListener('submit', formSubmitHandler);
 popup.addEventListener('click', clickOverlay)
-popupOpenBtn.addEventListener('click', popupOpen);
-popupCloseBtn.addEventListener('click', popupClose);
+popupOpenBtn.addEventListener('click', openPopup);
+popupCloseBtn.addEventListener('click', closePopup);
